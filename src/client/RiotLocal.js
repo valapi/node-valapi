36b889a600982561d3d0b5171fca4e829477efad
KT_NG3 <84909754+KTNG-3@users.noreply.github.com>
2022-04-18 17:35:33 +0700

    1.5.8

--- src/client/RiotLocal.js
+++ src/client/RiotLocal.js
@@ -123,7 +123,7 @@ class RiotLocal {
         var _string_endpoint = String(_endpoint);
         var _string_body = String(JSON.stringify(_body));
 
-        for (let i in data.replace) {
+        for (let i = 0; i < data.replace.length; i++) {
             const _change = data.replace[i];
             const _args = arguments[i + 1];
             if (_change.where === 'url') {
